07d5b7e00a9f03f5509a59c37bc554b1bbbac2da
KayLark12 <kaylark946@gmail.com>
2024-08-01 23:26:44 +0700

    index.js

--- index.js
+++ index.js
@@ -9,21 +9,47 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Periksa apakah semua variabel lingkungan ada
+const {
+  FIREBASE_TYPE,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_PRIVATE_KEY_ID,
+  FIREBASE_PRIVATE_KEY,
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_CLIENT_ID,
+  FIREBASE_AUTH_URI,
+  FIREBASE_TOKEN_URI,
+  FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  FIREBASE_CLIENT_X509_CERT_URL
+} = process.env;
+
+if (!FIREBASE_TYPE || !FIREBASE_PROJECT_ID || !FIREBASE_PRIVATE_KEY_ID || !FIREBASE_PRIVATE_KEY || 
+    !FIREBASE_CLIENT_EMAIL || !FIREBASE_CLIENT_ID || !FIREBASE_AUTH_URI || !FIREBASE_TOKEN_URI || 
+    !FIREBASE_AUTH_PROVIDER_X509_CERT_URL || !FIREBASE_CLIENT_X509_CERT_URL) {
+  console.error('Missing environment variables for Firebase Admin SDK');
+  process.exit(1);
+}
+
 // Inisialisasi Firebase Admin SDK menggunakan environment variables
 admin.initializeApp({
   credential: admin.credential.cert({
-    "type": process.env.FIREBASE_TYPE,
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "client_id": process.env.FIREBASE_CLIENT_ID,
-    "auth_uri": process.env.FIREBASE_AUTH_URI,
-    "token_uri": process.env.FIREBASE_TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+    type: FIREBASE_TYPE,
+    project_id: FIREBASE_PROJECT_ID,
+    private_key_id: FIREBASE_PRIVATE_KEY_ID,
+    private_key: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: FIREBASE_CLIENT_EMAIL,
+    client_id: FIREBASE_CLIENT_ID,
+    auth_uri: FIREBASE_AUTH_URI,
+    token_uri: FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: FIREBASE_CLIENT_X509_CERT_URL
   }),
   databaseURL: 'https://skripsi-tiket-ece21-default-rtdb.asia-southeast1.firebasedatabase.app'
+});
+
+// Rute root untuk memastikan server berjalan
+app.get('/', (req, res) => {
+  res.send('Server is running');
 });
 
 // Endpoint untuk menerima notifikasi Midtrans
